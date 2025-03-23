7c358e0104b3cb802cbbdcb89156e43734266ed7
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-03-23 16:58:07 +0530

    cros

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'http://resonant-tiramisu-2552d8.netlify.app',
     credentials: true
 }
 
