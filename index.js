04d41a9bfb9c70007a63731a1949acebd91b0574
Rajpravinkumar <rajpravinkumaras@gmail.com>
2025-03-24 14:17:41 +0530

    url

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https://resonant-tiramisu-2552d8.netlify.app',
+    origin: 'https://hireup-app.vercel.app',
     credentials: true
 }
 // https://resonant-tiramisu-2552d8.netlify.app/login
